0cc7b268a14ba503b078d0223f1e36289b1c166e
berlianaputri <42725251+berlianaputri@users.noreply.github.com>
2018-09-22 10:53:59 +0700

    Update footer.php

--- session/footer.php
+++ session/footer.php
@@ -36,8 +36,9 @@
 
 	<div style="background-color: #4dffff;" align="center">
 			<img src="koala.jpg" width="50">
-			<font size="10" color="black">Copyright.2018<br>Berliana Putri</font>
-			<img src="koala.jpg" width="50">
+			<<font size="10" color="black">&copyCopyright.2018 Berliana Putri</font>
+			<img src="koala.jpg" width="50"><br>
+			<font size="5" color="black"><a href="www.instagram.com/berlianaputrii_">Instagram</a></font>
 		<hr>
 	</div>
 </body>
